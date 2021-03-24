ec2d34de3ab37b7196546c0b80f55e9dc470f618
pseudoaconitine <26392038+pseudoaconitine@users.noreply.github.com>
2021-03-25 01:41:57 +0300

    Add basic modal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import React, { useState } from 'react';
 import Header from './components/Header.js';
 import TaskBoard from './pages/TaskBoard.js';
 import Login from './pages/Login.js';
 import SignUp from './pages/SignUp.js';
+import Modal from './components/Modal.js';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
 function App() {
+  const [show, setShow] = useState(false);
   return (
     <main className="App">
+      <button onClick={()=>{setShow(true)}}>Show Modal</button>
+      <Modal size="lg" onClose={()=>{setShow(false)}} show={show} >
+        <p>I am modal content</p>
+      </Modal>
       <Router>
       <Header/>
         <Switch>
---
+++ src/components/Modal.js
@@ -0,0 +1,25 @@
+import React,{useEffect} from 'react'
+import './Modal.css'
+function Modal(props) {
+    const closeOnEscapeKeyDown=(e)=>{
+        if((e.charCode||e.keyCode)===27)
+        props.onClose();
+    }
+    useEffect(()=>{
+        document.body.addEventListener('keydown',closeOnEscapeKeyDown)
+        return function cleanUp(){
+            document.body.removeEventListener('keydown',closeOnEscapeKeyDown)
+        }
+    })
+    
+    return (
+        <aside>
+            <div className={`overlay ${props.show ?"show":""}`} onClick={props.onClose}>&nbsp;</div>
+            <div className={`modal ${props.size} ${props.show ?"show":""}`} onClick={(e)=>e.stopPropagation()}>
+                <div className="modal-body">{props.children}</div>
+            </div>
+        </aside>
+    )
+}
+
+export default Modal
