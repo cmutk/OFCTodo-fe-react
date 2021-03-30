5e28891a50e6eb3cd4d51ea9c3bcc45b98c34d9c
pseudoaconitine <26392038+pseudoaconitine@users.noreply.github.com>
2021-03-30 21:40:33 +0300

    Add ability to add tasks to the local storage

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import React, {useState} from 'react';
+import React, {useState,useEffect} from 'react';
 import Navbar from './components/Navbar.js';
 import TaskBoard from './pages/TaskBoard.js';
 import Login from './pages/Login.js';
 import SignUp from './pages/SignUp.js';
-import NewTask from './components/NewTask.js'
+import NewTask from './pages/NewTask.js'
 import ModalLarge from './components/ModalLarge.js'
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
+
 function App() {  
   const [show, setShow] = useState(false);
   const closeModal=()=> {setShow(false)}
+  const LOCAL_STORAGE_KEY_TASKS = 'ofcourse.tasks';
+  const tasksOnLocalStorage = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_TASKS))
+  const [tasks, setTasks] = useState(tasksOnLocalStorage||[])
+ 
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY_TASKS, JSON.stringify(tasks))
+    
+  }, [tasks])
+
   return (
     <main className="App">
       <Router>
       <Navbar right={<button onClick={()=>{setShow(true)}}> New Task !</button>}/>
-      <ModalLarge content={<NewTask onClose={closeModal}/>} headerOf="newtask" title="New Task" show={show} onClose={closeModal}/>
+      <ModalLarge content={<NewTask onClose={closeModal} setTasks={setTasks}/>} headerOf="newtask" title="New Task" show={show} onClose={closeModal}/>
         <Switch>
           <Route path="/" exact component={TaskBoard} />
           <Route path="/login" component={Login} />
--- src/pages/NewTask.js
+++ src/pages/NewTask.js
@@ -1,42 +1,63 @@
-import React,{useState} from 'react';
+import React,{useState,useRef,useEffect} from 'react';
 import './NewTask.css';
-import InputContainer from './InputContainer.js'
+import InputContainer from '../components/InputContainer.js';
 import { IoToggleSharp } from "react-icons/io5";
+import {v4 as uuidv4} from 'uuid';
 function NewTask(props) {
   const [showContent, setShowContent] = useState(false);
   const toggle =(currentState)=>{
     const val = (currentState ===false)?true:false
     return setShowContent(val)
+  }
+  
+  const taskNameInput= useRef(null);
+  const startDateInput= useRef(null);
+  const deadlineInput= useRef(null);
+  const descriptionInput = useRef(null);
+
+ 
+  
+  const storeTask =(setTasks)=>{
+    const name=taskNameInput.current.value;
+    const startsFrom=startDateInput.current.value;
+    const deadline=deadlineInput.current.value;
+    const description =descriptionInput.current.value;
+    setTasks(prevTasks=>{
+      return [...prevTasks,{id:uuidv4(), name:name, startsFrom:startsFrom, deadline:deadline, description:description}]
+    })
     
+
   }
     return (
-        <>
+        <form onSubmit={(e)=>{e.preventDefault(); storeTask(props.setTasks)}}>
         <div className="newtask-container">
           <h4 className="newtask-options-info">Basic</h4>
           <InputContainer title="Task Name" message="Some Error Message" status={"default"}>
-            <input type="text" id="task-name"/>
+            <input ref={taskNameInput} type="text" id="task-name"/>
             </InputContainer>
             <InputContainer title="Start Date" message="Some Error Message" status={"error"}>
-            <input type="date" id="start-date"/>
+            <input ref={startDateInput} type="date" id="start-date"/>
             </InputContainer>
             <InputContainer title="Deadline" message="Some Error Message" status={"valid"}>
-            <input type="date" id="deadline"/>
+            <input ref={deadlineInput} type="date" id="deadline"/>
             </InputContainer>
           <h4 className="newtask-options-info">Detailed</h4>
          
            <label className="toggle-switch-container" htmlFor="description" onClick={()=>{toggle(showContent)}}>Description
            <div className="toggle-icon-container"><IoToggleSharp className={`toggle-icon ${showContent?"on":""}`}/></div>
            </label>
-          <div className={`toggle-content ${showContent?"show":""}`}><textarea name="description" id="description" ></textarea></div> 
+          <div className={`toggle-content ${showContent?"show":""}`}>
+            <textarea ref={descriptionInput} name="description" id="description" ></textarea>
+            </div> 
            <label className="toggle-switch-container" htmlFor="subtasks">Subtasks
            <div className="toggle-icon-container"><IoToggleSharp className="toggle-icon"/></div></label>
            <div className="button-container">
-           <button className="btn-newtask create">Create!</button>
+           <button type="submit"className="btn-newtask create">Create!</button>
             <button className="btn-newtask cancel" onClick={props.onClose}>Cancel</button>
            </div>
             
         </div>
-        </>
+        </form>
     )
 }
 
